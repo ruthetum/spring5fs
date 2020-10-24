1e6cc8a33e7ebe8f822dc05c6a983c804442b10f
Heedong Kim <ruthetum@gmail.com>
2020-10-24 19:32:07 +0900

    Update MemberPrinter.java
    자동 주입할 대상이 필수가 아닌 경우
    1. @Autowired(required = false)
    2. Optional<>
    3. @Nullable

--- ch04/src/main/java/DI/MemberPrinter.java
+++ ch04/src/main/java/DI/MemberPrinter.java
@@ -1,13 +1,44 @@
 package DI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
 public class MemberPrinter {
+    @Autowired(required = false)
+    private DateTimeFormatter dateTimeFormatter;
+
+//    @Autowired
+//    private Optional<DateTimeFormatter> opt;
+
+//    @Autowired
+//    @Nullable
+//    private DateTimeFormatter dateTimeFormatter;
+
     public void print(Member member){
-        System.out.printf(
+        if (dateTimeFormatter == null) {
+            System.out.printf(
                 "회원 정보 : 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n",
                 member.getId(),
                 member.getEmail(),
                 member.getName(),
                 member.getRegisterDateTime()
-        );
+            );
+        } else {
+            System.out.printf(
+                "회원 정보 : 아이디=%d, 이메일=%s, 이름=%s, 등록일=%s\n",
+                member.getId(),
+                member.getEmail(),
+                member.getName(),
+                dateTimeFormatter.format(member.getRegisterDateTime())
+            );
+        }
+    }
+
+    @Autowired(required = false)
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
     }
 }

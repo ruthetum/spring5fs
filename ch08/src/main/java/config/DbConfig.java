fdb3e214b168e45d505fd709c9b1890f6c28c22c
Heedong Kim <ruthetum@gmail.com>
2020-10-27 06:14:23 +0900

    [ch08] Data Source 및 커넥션 풀 설정

---
+++ ch08/src/main/java/config/DbConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2); // 커넥션 풀 초기화 2개
+        ds.setMaxActive(10); // 풀 최대 10개
+        ds.setTestWhileIdle(true); // 유휴 커넥션 검사
+        ds.setMinEvictableIdleTimeMillis(1000 * 60 * 3); // 최소 유휴 시간 3분
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 10); // 10초 주기로 검사
+        return ds;
+    }
+}

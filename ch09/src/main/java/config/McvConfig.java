65ba929e27c152859deec091e5ee0f433a25ee44
Heedong Kim <ruthetum@gmail.com>
2020-10-30 01:12:58 +0900

    [ch09] Spring MVC 초기 설정
    - pom.xml/gradle 설정
    - @EnableWebMvc
    - web.xml에서 DispatcherServlet 설정

---
+++ ch09/src/main/java/config/McvConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class McvConfig implements WebMvcConfigurer {
+
+    // DispatcherServlet 매핑 경로를 '/'로 주었을 때 처리하기 위한 설정
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    // JSP 경로 설정
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB_INF/view/", ".jsp");
+    }
+}
